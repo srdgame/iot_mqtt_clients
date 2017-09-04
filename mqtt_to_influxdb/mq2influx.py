1e4483874a8b5a5f172d72170c8e754f1e9e4138
Dirk Chang <dirk@kooiot.com>
2017-09-04 13:41:03 +0800

    redis database correction

--- mqtt_to_influxdb/mq2influx.py
+++ mqtt_to_influxdb/mq2influx.py
@@ -22,8 +22,8 @@ match_stat_path = re.compile(r'^([^/]+)/([^/]+)/(.+)$')
 config = ConfigParser()
 config.read('../config.ini')
 
-redis_srv = config.get('redis', 'url', fallback='redis://127.0.0.1:6379')
-redis_db = redis.Redis.from_url(redis_srv+"/8")
+redis_srv = config.get('redis', 'url', fallback='redis://127.0.0.1:6379') + "/8"
+redis_db = redis.Redis.from_url(redis_srv)
 
 workers = {}
 device_map = {}
